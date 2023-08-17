24cf8f221ac4c413355b60df5989a26752afbbf5
Daniel Mwaniki <mwaniel03@gmail.com>
2023-08-17 10:41:26 +0300

    user controler

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
+
 class UserController extends Controller
 {
     public function register(Request $request)
@@ -27,16 +28,21 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
+        // Validate user credentials
         $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required|string',
         ]);
 
+        // Attempt authentication
         if (auth()->attempt($credentials)) {
+            // User authentication successful
             $user = auth()->user();
             $token = $user->createToken('authToken')->plainTextToken;
+
             return response()->json(['token' => $token], 200);
         } else {
+            // User authentication failed
             return response()->json(['message' => 'Unauthorized'], 401);
         }
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,17 +4,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     protected $casts = [
@@ -26,3 +30,4 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 }
+
