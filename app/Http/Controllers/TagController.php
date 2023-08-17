912e25a04576ab11e12ac331ff51165602da6159
Daniel Mwaniki <mwaniel03@gmail.com>
2023-08-17 11:57:42 +0300

    tag add

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -17,5 +17,18 @@ class TagController extends Controller
     {
         return response()->json($tag);
     }
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|unique:tags|max:255',
+        ]);
+
+        $tag = new Tag();
+        $tag->name = $validatedData['name'];
+        $tag->save();
+
+        return response()->json($tag, 201);
+    }
+
 }
 
